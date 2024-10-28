bb0e72b955048431ba578da1e450e146e21690ad
AL Bahaar Islamic Academy <haroon121387@gmail.com>
2024-10-28 16:49:56 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-// main.js
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('nav');
 
-    hamburger.addEventListener('click', () => {
-        nav.classList.toggle('show'); // Toggle the 'show' class
-        hamburger.classList.toggle('active'); // Toggle active class for animation
-    });
-});
