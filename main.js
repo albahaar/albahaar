6a17a1170b7ba9b36c1f1ec2d0653ee790a6a5ed
AL Bahaar Islamic Academy <haroon121387@gmail.com>
2024-10-28 16:41:55 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-// Function to toggle the menu
-function toggleMenu() {
-    const nav = document.getElementById("navbar");
-    nav.classList.toggle("show");
-}
+// main.js
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
+
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('show'); // Toggle the 'show' class
+        hamburger.classList.toggle('active'); // Toggle active class for animation
+    });
+});
